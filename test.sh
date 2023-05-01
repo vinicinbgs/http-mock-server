1dc5b31336a976a6e34ea35d9b78e84b7d393193
Vinicius Morais Dutra <vmdutra@rd.com.br>
2023-05-01 00:26:49 -0300

    chore(makefile): add makefile commands {request, test, clean}

--- test.sh
+++ test.sh
@@ -5,6 +5,8 @@
 RUSTFLAGS="-C instrument-coverage" cargo test --tests
 grcov . -s . --binary-path ./target/debug/ -t html --branch -o ./target/debug/coverage/
 
+path=$(pwd)
+echo "Openning in browser! `open file://$path/target/debug/coverage/index.html`"
 ##### Alternative #####
 # brew install lcov
 # grcov . -s . --binary-path ./target/debug/ -t lcov --branch --ignore-not-existing -o ./target/debug/coverage/
