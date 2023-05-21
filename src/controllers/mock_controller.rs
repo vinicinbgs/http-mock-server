fe339262abc94b148a9f30ee7ad64d67b53b1894
Vinicius Morais Dutra <viniciusmd@ciandt.com>
2023-05-21 11:29:56 -0300

    feat(byte): add stream byte file mechanism

--- src/controllers/mock_controller.rs
+++ src/controllers/mock_controller.rs
@@ -1,77 +1,16 @@
 use crate::server::HttpFields;
-use serde_json::Value::{self, Null};
+use serde_json::Value;
 use std::{io::Write, net::TcpStream};
+
+#[macro_use]
+#[path = "./macros/response_status.rs"]
+mod response_status;
 
 #[path = "../services/mock.rs"]
 mod mock_service;
-use mock_service::{File, Http};
-
-macro_rules! status {
-    ($a: expr, $b:expr) => {
-        match $a {
-            "200" => "HTTP/1.1 200 OK",
-            "201" => "HTTP/1.1 201 CREATED",
-            "202" => "HTTP/1.1 202 ACCEPTED",
-            "203" => "HTTP/1.1 203 NON-AUTHORITATIVE INFORMATION",
-            "204" => "HTTP/1.1 204 NO CONTENT",
-            "205" => "HTTP/1.1 205 RESET CONTENT",
-            "206" => "HTTP/1.1 206 PARTIAL CONTENT",
-            "300" => "HTTP/1.1 300 MULTIPLE CHOICES",
-            "301" => "HTTP/1.1 301 MOVED PERMANENTLY",
-            "302" => "HTTP/1.1 302 FOUND",
-            "303" => "HTTP/1.1 303 SEE OTHER",
-            "304" => "HTTP/1.1 304 NOT MODIFIED",
-            "305" => "HTTP/1.1 305 USE PROXY",
-            "307" => "HTTP/1.1 307 TEMPORARY REDIRECT",
-            "308" => "HTTP/1.1 308 PERMANENT REDIRECT",
-            "400" => "HTTP/1.1 400 BAD REQUEST",
-            "401" => "HTTP/1.1 401 UNAUTHORIZED",
-            "402" => "HTTP/1.1 402 PAYMENT REQUIRED",
-            "403" => "HTTP/1.1 403 FORBIDDEN",
-            "404" => "HTTP/1.1 404 NOT FOUND",
-            "405" => "HTTP/1.1 405 METHOD NOT ALLOWED",
-            "406" => "HTTP/1.1 406 NOT ACCEPTABLE",
-            "407" => "HTTP/1.1 407 PROXY AUTHENTICATION REQUIRED",
-            "408" => "HTTP/1.1 408 REQUEST TIMEOUT",
-            "409" => "HTTP/1.1 409 CONFLICT",
-            "410" => "HTTP/1.1 410 GONE",
-            "411" => "HTTP/1.1 411 LENGTH REQUIRED",
-            "412" => "HTTP/1.1 412 PRECONDITION FAILED",
-            "413" => "HTTP/1.1 413 PAYLOAD TOO LARGE",
-            "414" => "HTTP/1.1 414 URI TOO LONG",
-            "415" => "HTTP/1.1 415 UNSUPPORTED MEDIA TYPE",
-            "416" => "HTTP/1.1 416 RANGE NOT SATISFIABLE",
-            "417" => "HTTP/1.1 417 EXPECTATION FAILED",
-            "418" => "HTTP/1.1 418 I'M A TEAPOT",
-            "421" => "HTTP/1.1 421 MISDIRECTED REQUEST",
-            "422" => "HTTP/1.1 422 UNPROCESSABLE ENTITY",
-            "423" => "HTTP/1.1 423 LOCKED",
-            "424" => "HTTP/1.1 424 FAILED DEPENDENCY",
-            "425" => "HTTP/1.1 425 TOO EARLY",
-            "426" => "HTTP/1.1 426 UPGRADE REQUIRED",
-            "428" => "HTTP/1.1 428 PRECONDITION REQUIRED",
-            "429" => "HTTP/1.1 429 TOO MANY REQUESTS",
-            "431" => "HTTP/1.1 431 REQUEST HEADER FIELDS TOO LARGE",
-            "451" => "HTTP/1.1 451 UNAVAILABLE FOR LEGAL REASONS",
-            "500" => "HTTP/1.1 500 INTERNAL SERVER ERROR",
-            "501" => "HTTP/1.1 501 NOT IMPLEMENTED",
-            "502" => "HTTP/1.1 502 BAD GATEWAY",
-            "503" => "HTTP/1.1 503 SERVICE UNAVAILABLE",
-            "504" => "HTTP/1.1 504 GATEWAY TIMEOUT",
-            "505" => "HTTP/1.1 505 HTTP VERSION NOT SUPPORTED",
-            "506" => "HTTP/1.1 506 VARIANT ALSO NEGOTIATES",
-            "507" => "HTTP/1.1 507 INSUFFICIENT STORAGE",
-            "508" => "HTTP/1.1 508 LOOP DETECTED",
-            "510" => "HTTP/1.1 510 NOT EXTENDED",
-            "511" => "HTTP/1.1 511 NETWORK AUTHENTICATION REQUIRED",
-            _ => $b,
-        }
-    };
-}
+use mock_service::{Http, MockFile, TypeOr};
 
 pub fn mock(stream: TcpStream, http_fields: HttpFields) {
-    let mut status: &str = "HTTP/1.1 200 OK";
-
     let http_path = http_fields.original_url.as_str();
     let http_method = http_fields.method.as_str();
 
@@ -87,37 +26,53 @@ pub fn mock(stream: TcpStream, http_fields: HttpFields) {
             method: http_method,
             request_body: http_fields.body,
         },
-        File {
+        MockFile {
             file_path: String::new(),
         },
     );
 
-    if ret["$.body"]["path"] != Null {
-        status = "HTTP/1.1 404 NOT FOUND";
+    match ret {
+        TypeOr::Left(ret) => {
+            return stream_response(&stream, ret, String::from("200"));
+        }
+        TypeOr::Right(ret) => {
+            return response(
+                &stream,
+                ret["$.body"].to_owned(),
+                ret["$.status"].to_string(),
+            );
+        }
     }
-
-    if ret["$.body"]["request"] != Null {
-        status = "HTTP/1.1 400 BAD REQUEST";
-    }
-
-    return response(
-        &stream,
-        ret["$.body"].to_owned(),
-        status!(ret["$.status"].to_string().as_str(), status),
-    );
 }
 
-fn response_format(status: String, length: String, content: String) -> String {
-    let content_type: &str = "Content-Type: application/json";
+fn response_format(_status: String, _length: String, _content_type: String) -> String {
+    let content_type: &str =
+        "Content-Type: {_content_type}\r\nAccess-Control-Allow-Origin: *\r\nAccept-Ranges: bytes";
 
-    return format!("{status}\r\n{length}\r\n{content_type}\r\n\r\n{content}");
+    let status = status!(_status.as_str());
+
+    return format!("{status}\r\n{_length}\r\n{content_type}\r\n\r\n");
 }
 
-fn response(mut stream: &TcpStream, data: Value, status: &str) {
+fn stream_response(mut stream: &TcpStream, data: Vec<u8>, status: String) {
+    let length = format!("Content-Length: {}", data.len());
+    let response = response_format(status, length, "video/mp4".to_string());
+
+    stream.write_all(response.as_bytes()).unwrap();
+
+    stream.write_all(&data).unwrap();
+
+    stream.flush().unwrap();
+}
+
+fn response(mut stream: &TcpStream, data: Value, status: String) {
     let content = format!("{}", data);
     let length = format!("Content-Length: {}", content.len());
+    let response = response_format(status, length, "application/json".to_string());
 
-    stream
-        .write_all(response_format(status.to_string(), length, content).as_bytes())
-        .unwrap();
+    stream.write_all(response.as_bytes()).unwrap();
+
+    stream.write(content.to_string().as_bytes()).unwrap();
+
+    stream.flush().unwrap();
 }
--- src/services/mock.rs
+++ src/services/mock.rs
@@ -3,17 +3,21 @@ use serde_json::{
     Value::{self, Null},
 };
 
-use std::{env, fs};
+use std::{
+    env,
+    fs::{self, File},
+    io::Read,
+};
 
 trait DataFile {
     fn get_path(&self) -> String;
 }
 
-pub struct File {
+pub struct MockFile {
     pub file_path: String,
 }
 
-impl DataFile for File {
+impl DataFile for MockFile {
     fn get_path(&self) -> String {
         let args: Vec<String> = env::args().collect();
 
@@ -49,7 +53,12 @@ pub struct Http<'a> {
     pub request_body: String,
 }
 
-pub fn execute(http: Http, file: File) -> Value {
+pub enum TypeOr<S, T> {
+    Left(S),
+    Right(T),
+}
+
+pub fn execute(http: Http, file: MockFile) -> TypeOr<Vec<u8>, Value> {
     let file_path = file.get_path();
 
     let file_string = fs::read_to_string(&file_path)
@@ -60,13 +69,14 @@ pub fn execute(http: Http, file: File) -> Value {
     let data_request_body = data[http.path][http.method]["$.request"].to_owned();
 
     if data[http.path][http.method] == Null {
-        return json!({
+        return TypeOr::Right(json!({
             "$.body": {
                 "error": "URI Path or HTTP Method Not found",
                 "path": http.path,
                 "method": http.method,
-            }
-        });
+            },
+            "$.status": "404",
+        }));
     }
 
     if check_http_request_body_is_different_from_data_request_body(
@@ -77,12 +87,13 @@ pub fn execute(http: Http, file: File) -> Value {
         http.request_body.to_string(),
     ) {
         let request: Value = serde_json::from_str(&http.request_body).unwrap_or_default();
-        return json!({
+        return TypeOr::Right(json!({
             "$.body": {
                 "error": "Request body does not match",
                 "request": request,
-            }
-        });
+            },
+            "$.status": "400",
+        }));
     }
 
     let _ = &data[http.path][http.method]
@@ -90,7 +101,23 @@ pub fn execute(http: Http, file: File) -> Value {
         .unwrap()
         .remove("$.request");
 
-    return data[http.path][http.method]["$.response"].to_owned();
+    if data[http.path][http.method]["$.response"]["$.file"] != Null {
+        let filename = data[http.path][http.method]["$.response"]["$.file"]
+            .as_str()
+            .unwrap();
+
+        let mut f = match File::open(&filename) {
+            Ok(file) => file,
+            Err(_) => panic!("Unable to open asset file {}", filename),
+        };
+
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer).unwrap();
+
+        return TypeOr::Left(buffer);
+    }
+
+    return TypeOr::Right(data[http.path][http.method]["$.response"].to_owned());
 }
 
 fn check_http_request_body_is_different_from_data_request_body(
@@ -120,12 +147,17 @@ mod tests {
                 method: "POST",
                 request_body: "".to_string(),
             },
-            File {
+            MockFile {
                 file_path: "src/services/test_mock_data.json".to_string(),
             },
         );
 
-        assert_eq!(ret["$.body"]["name"], "John Doe");
+        match ret {
+            TypeOr::Left(_) => panic!("Should not return binary data"),
+            TypeOr::Right(ret) => {
+                assert_eq!(ret["$.body"]["name"], "John Doe");
+            }
+        }
     }
 
     #[test]
@@ -136,12 +168,17 @@ mod tests {
                 method: "GET",
                 request_body: "".to_string(),
             },
-            File {
+            MockFile {
                 file_path: "src/services/test_mock_data.json".to_string(),
             },
         );
 
-        assert_eq!(ret["$.body"]["error"], "URI Path or HTTP Method Not found");
+        match ret {
+            TypeOr::Left(_) => panic!("Should not return binary data"),
+            TypeOr::Right(ret) => {
+                assert_eq!(ret["$.body"]["error"], "URI Path or HTTP Method Not found");
+            }
+        }
     }
 
     #[test]
@@ -152,12 +189,17 @@ mod tests {
                 method: "POST",
                 request_body: r#"{"name": "what_your_name"}"#.to_string(),
             },
-            File {
+            MockFile {
                 file_path: "src/services/test_mock_data.json".to_string(),
             },
         );
 
-        assert_eq!(ret["$.body"]["error"], "Request body does not match");
+        match ret {
+            TypeOr::Left(_) => panic!("Should not return binary data"),
+            TypeOr::Right(ret) => {
+                assert_eq!(ret["$.body"]["error"], "Request body does not match");
+            }
+        }
     }
 
     #[test]
@@ -168,12 +210,17 @@ mod tests {
                 method: "POST",
                 request_body: "".to_string(),
             },
-            File {
+            MockFile {
                 file_path: "-f=src/services/test_mock_data.json".to_string(),
             },
         );
 
-        assert_eq!(ret["$.body"]["name"], "John Doe");
+        match ret {
+            TypeOr::Left(_) => panic!("Should not return binary data"),
+            TypeOr::Right(ret) => {
+                assert_eq!(ret["$.body"]["name"], "John Doe");
+            }
+        }
     }
 
     #[test]
@@ -185,7 +232,7 @@ mod tests {
                 method: "POST",
                 request_body: "".to_string(),
             },
-            File {
+            MockFile {
                 file_path: "file_that_not_exist.json".to_string(),
             },
         );
@@ -199,11 +246,18 @@ mod tests {
                 method: "POST",
                 request_body: "".to_string(),
             },
-            File {
+            MockFile {
                 file_path: "./src/services/test_mock_data.json".to_string(),
             },
         );
 
-        assert_eq!(ret["$.body"]["name"], "John Doe");
+        match ret {
+            TypeOr::Left(_) => panic!("Should not return binary data"),
+            TypeOr::Right(ret) => {
+                assert_eq!(ret["$.body"]["name"], "John Doe");
+            }
+        }
     }
 }
+
+// todo: rename file to match_mock
